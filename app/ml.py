3f157cb58c099ecfb9a80e024f197d631b62766d
Flora Xu <flora.xushujing@gmail.com>
2022-12-29 15:29:41 -0800

    finished app locally

---
+++ app/ml.py
@@ -0,0 +1,46 @@
+import os
+
+import torch
+from diffusers import StableDiffusionPipeline
+from PIL.Image import Image
+
+token = os.getenv("HUGGINGFACE_TOKEN")
+
+# get your token at https://huggingface.co/settings/tokens
+model_id = "runwayml/stable-diffusion-v1-5" # "CompVis/stable-diffusion-v1-4"
+device = "mps" # "cuda"
+
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    pretrained_model_name_or_path = model_id,
+    revision="fp16",
+    torch_dtype=torch.float16,
+    use_auth_token=token,
+)
+pipe = pipe.to(device)
+pipe.enable_attention_slicing() # < 64 GB of Ram
+
+# prompt = "a photo of an astronaut riding a horse on mars"
+# image = pipe(prompt).images[0]      
+# image.save("astronaut_rides_horse.png")
+
+def obtain_image(
+    prompt: str,
+    *,
+    seed: int | None = None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5,
+) -> Image:
+    generator = None if seed is None \
+        else torch.Generator().manual_seed(seed)
+    print(f"Using device: {pipe.device}")
+    image: Image = pipe(
+        prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        generator=generator,
+    ).images[0]
+    return image
+
+
+# image = obtain_image(prompt, num_inference_steps=2, seed=1024)
