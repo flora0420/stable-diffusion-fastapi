52ec4677b9fbd45933f3002a3a542e290bdeba23
Flora Xu <flora.xushujing@gmail.com>
2022-12-29 17:10:05 -0800

    build docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.10-slim 
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+
+RUN pip install --upgrade -r /code/requirements.txt
+
+COPY ./app /code/app
+
+CMD ["uvicorn", "app.main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse, StreamingResponse
 from pydantic import BaseModel
 
-from ml import obtain_image
+from .ml import obtain_image
 
 app = FastAPI()
 
@@ -14,20 +14,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int):
-#     return {"item_id": item_id}
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
 
 
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     tags: list[str] = []
+class Item(BaseModel):
+    name: str
+    price: float
+    tags: list[str] = []
 
 
-# @app.post("/items/")
-# def create_item(item: Item):
-#     return item
+@app.post("/items/")
+def create_item(item: Item):
+    return item
 
 
 @app.get("/generate")
